d351fcb0eab448e94ad47af5bee8300cfeab1374
VgionTech <vgion.tech@gmail.com>
2021-07-10 22:27:36 +0530

    init

---
+++ app/src/main/java/com/example/navigationcomponentbottomnavigation/FragmentTwo.java
@@ -0,0 +1,36 @@
+package com.example.navigationcomponentbottomnavigation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    private View rootView;
+    private Toolbar toolbar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_two, container, false);
+        toolbar = rootView.findViewById(R.id.toolbar);
+
+        NavController navController = Navigation.findNavController(getActivity(),
+                R.id.fragmentContainerView);
+        AppBarConfiguration appBarConfiguration =
+                ToolbarManager.getInstance().setTopLevelDestinationsBottomNavigation();
+        ToolbarManager.getInstance().setupToolbar(getActivity(), navController,
+                appBarConfiguration, toolbar, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponentbottomnavigation/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.navigationcomponentbottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}
